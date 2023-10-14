005a926f1f94108be983631162ddf5918a0e969d
comboni <fratmccj@comboni.org>
2023-10-14 10:00:32 -0600

    Updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function(){
 
 const init = function(){
 	let items = document.querySelectorAll('section');
-	let coor = document.getElementById('coor');	
+	// let coor = document.getElementById('coor');	
 	const scrollElement = document.getElementById('slider');
 	const pics = document.getElementsByClassName('imgsoap')
 	winWidth = window.outerWidth
@@ -12,7 +12,7 @@ const init = function(){
 	totalWidth = winWidth * items.length
 	// coor.innerHTML = winX;
 	// document.getElementById('coor').innerHTML += '<br>Some new content!';
-	console.log(coor)
+	// console.log(coor)
 
 // Get a reference to the element you want to monitor for horizontal scroll (e.g., the window or a specific div).
 
@@ -30,6 +30,9 @@ scrollElement.addEventListener('scroll', function () {
   console.log('active section:', actsec)
   console.log(pics)
 //   pics[actsec-1].classList.add('jspic')
+
+
+// Animation Section 2 =============================
 	if(actsec > 1.9 && actsec < 2.1){
 		console.log('ok - 2')
 		// document.getElementById('soap3').classList.add('jspic')
@@ -39,15 +42,21 @@ scrollElement.addEventListener('scroll', function () {
 			translateY: 250
 		  });
 	}
+
+
+
+
+
 });
 
+// Logo animation
 anime({
 	targets: '.fpart',
 	opacity: 1,
 	duration: 10000,
-	delay: anime.stagger(300) // increase delay by 100ms for each elements.
+	delay: anime.stagger(100) // increase delay by 100ms for each elements.
   });
-
+// Animation text rugiada
 anime({
   targets: '.anchor',
   strokeDashoffset: [anime.setDashoffset, 0],
@@ -60,6 +69,20 @@ anime({
             targets: '.moving',
             translateX: 1000,
         });
+
+// Section 13 ==Animation L'amore fa belle tutte le cose
+const masks = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25']
+masks.forEach((mask, index, el) => {
+  const id = `#mask-${mask}` // Prepend #mask- to each mask element name
+  let path = document.querySelector(id)
+  const length = path.getTotalLength() // Calculate the length of a path
+  path.style.strokeDasharray = length; // Set the length to stroke-dasharray in the styles
+  path.style.strokeDashoffset = length; // Set the length to stroke-dashoffset in the styles
+})
+
+
+
+
 
 const moving = document.getElementById('moving')
 console.log(moving)
